8f13b0aa92d68ea55bf3ff3d47c7c9fd7597c18f
kombo <1720056985@qq.com>
2019-11-26 19:38:15 +0800

    完整版v1.3

--- common/utils.py
+++ common/utils.py
@@ -9,8 +9,8 @@ def init_driver():
         "platformName": "Android",  # 模拟器名称
         "platformVersion": "5.1",  # 模拟器型号
         "deviceName": "Android 5",  # 模拟器平台
-        "appPackage": "com.bjcsxq.chat.carfriend",  # 引用的包名
-        "appActivity": ".MainActivity",  # 应用的启动名
+        "appPackage": "com.bjcsxq.chat.carfriend",  # 待测引用的包名
+        "appActivity": ".MainActivity",  # 待测 应用的启动名
         "noReset": True
     }
     driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_capabilities=capabilities)
