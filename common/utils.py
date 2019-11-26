4a2be08b36b8e63fe70f41e5bb87313d448d1d83
kombo <1720056985@qq.com>
2019-11-26 20:55:14 +0800

    完整版v1.4

--- common/utils.py
+++ common/utils.py
@@ -11,7 +11,8 @@ def init_driver():
         "deviceName": "Android 5",  # 模拟器平台
         "appPackage": "com.bjcsxq.chat.carfriend",  # 待测引用的包名
         "appActivity": ".MainActivity",  # 待测 应用的启动名
-        "noReset": True
+        "resetKeyboard": True,
+        "unicodeKeyboard": True
     }
     driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_capabilities=capabilities)
     driver.implicitly_wait(30)
--- page/__init__.py
+++ page/__init__.py
@@ -7,7 +7,7 @@ login_btn = By.XPATH, '//*[contains(@text,"登录")]'
 # 账号
 user_name = By.XPATH,'//*[contains(@text,"账号")]'
 # 密码
-pwd = By.ID,'com.bjcsxq.chat.carfriend:id/login_btn'
+pwd = By.ID, 'com.bjcsxq.chat.carfriend:id/login_pwd_et'
 # 登录按钮
 login_bu = By.ID, 'com.bjcsxq.chat.carfriend:id/login_btn'
 # 确定
